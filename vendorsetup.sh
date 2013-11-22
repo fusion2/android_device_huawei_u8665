25e90c29f2ba7873784fb69434689ea0574fd0fb
cameron581 <cameron581@cox.net>
2013-11-22 08:59:10 -0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,22 @@
-# This whole script is a hack. Hoping to replace it soon.
-if [ ! -z "$JENKINS_HOME" ]
-then
-  DEVICE_NAME=${LUNCH#*_} DEVICE_NAME=${DEVICE_NAME%-*}
-  if [ "$DEVICE_NAME" == "u8815" ]
-  then
-    sh device/huawei/msm7x27a-common/patches/apply.sh
-    sh device/huawei/u8815/patches/apply.sh
-  fi
-fi
+#
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+add_lunch_combo u8665-userdebug
+add_lunch_combo u8665-eng
+add_lunch_combo cm_u8665-userdebug
+
+# Run device patches on the tree.
+sh device/huawei/u8815/patches/apply.sh
