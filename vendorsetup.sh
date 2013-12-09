ffff32cc76facbeaa78f809a6e0c44f2b241e8af
cameron581 <cameron581@cox.net>
2013-12-09 17:35:58 -0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,6 +22,3 @@ add_lunch_combo cm_u8665-userdebug
 # Run device patches on the tree.
 sh device/huawei/u8665/patches/apply.sh
 sh device/huawei/msm7x27a-common/patches/apply.sh
-
-# Run get-prebuilts incase i forget
-sh vendor/cm/get-prebuilts
